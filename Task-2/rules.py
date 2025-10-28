bba1c586a80651b376cc9ef50b5892bfe55d541a
Danan Subramaniam <danan_varo@hotmail.com>
2025-10-28 13:37:50 +0100

    Created the rules.py and created base rules

---
+++ Task-2/rules.py
@@ -0,0 +1,45 @@
+
+
+def rules(inGrid):
+
+    # Save the row and column length
+    arryRowLen = len(inGrid)
+    arrColLen = len(inGrid[0])
+
+
+    newGrid = []
+
+    # Create a new empty grid with the same size 
+    for i in range(arryRowLen):
+        row =[]
+        for j in range(arrColLen):
+            row.append(0)
+        newGrid.append(row)
+
+    for k in range(arryRowLen):
+        for m in range(arrColLen):
+            neighbor = 0
+
+
+
+
+
+
+
+
+    return 
+    
+        
+
+
+
+
+
+testGrid =  [
+    [0, 1, 0],
+    [0, 1, 0],
+    [0, 1, 0]
+    ]
+
+
+rules(testGrid)

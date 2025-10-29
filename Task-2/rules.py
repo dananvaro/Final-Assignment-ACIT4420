2eaafdf9b75f0273ee9f06b1a0b72e1fef49e17a
Danan Subramaniam <danan_varo@hotmail.com>
2025-10-29 14:16:19 +0100

    Logic done, missing main, grid and error

--- Task-2/rules.py
+++ Task-2/rules.py
@@ -1,33 +1,54 @@
 
 
+
+# Survival: A live cell with two or three live neighbors lives on to the next generation.
+# Death by underpopulation: A live cell with fewer than two live neighbors dies.
+# Death by overpopulation: A live cell with more than three live neighbors dies.
+# Birth: A dead cell with exactly three live neighbors becomes a live cell in the next generation.
+
 def rules(inGrid):
 
     # Save the row and column length
     arryRowLen = len(inGrid)
     arrColLen = len(inGrid[0])
 
-
     newGrid = []
 
+    directions = [(0,1),(1,0),(0,-1),(-1,0),(1,1),(-1,-1),(1,-1),(-1,1)]
+
     # Create a new empty grid with the same size 
-    for i in range(arryRowLen):
+    for _ in range(arryRowLen):
         row =[]
-        for j in range(arrColLen):
+        for _ in range(arrColLen):
             row.append(0)
         newGrid.append(row)
 
-    for k in range(arryRowLen):
-        for m in range(arrColLen):
-            neighbor = 0
+    for i in range(arryRowLen):
+        for j in range(arrColLen):
+            aliveNeigbour = 0
+            
+            # Counts arround the cell to find alive cells
+            for row, col in directions:
+                ix = row + i
+                jy = col + j
 
+                if((0 <= ix < arryRowLen and 0 <= jy < arrColLen) and (inGrid[ix][jy] == 1)):
+                    aliveNeigbour+= 1
 
+            # Death by underpopulation: A live cell with fewer than two live neighbors dies.
+            # Death by overpopulation: A live cell with more than three live neighbors dies.
+            if((aliveNeigbour < 2 or aliveNeigbour > 3) and inGrid[i][j] == 1):
+                newGrid[i][j] = 0
+            
+            # Birth: A dead cell with exactly three live neighbors becomes a live cell in the next generation.
+            elif(aliveNeigbour == 3 and inGrid[i][j] == 0):
+                newGrid[i][j] = 1
 
+            # Survival: A live cell with two or three live neighbors lives on to the next generation.
+            elif((aliveNeigbour == 2 or aliveNeigbour == 3) and inGrid[i][j]== 1):
+                newGrid[i][j] = 1
 
-
-
-
-
-    return 
+    return newGrid
     
         
 
@@ -35,11 +56,19 @@ def rules(inGrid):
 
 
 
-testGrid =  [
-    [0, 1, 0],
-    [0, 1, 0],
-    [0, 1, 0]
-    ]
+testGrid = [
+    [0,0,0,0,0],
+    [0,0,0,0,0],
+    [1,1,1,1,1],
+    [0,0,0,0,0],
+    [0,0,0,0,0],
+]
 
+for _ in range(25):
+    newGrid = rules(testGrid)
 
-rules(testGrid)
+    for row in newGrid: 
+        print(row)
+
+    print("---------")
+    testGrid = newGrid
